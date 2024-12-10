fd1782362a803f543b709dd3b8a05ac3e2867b4b
Dmitriy <arcon280506@mail.ru>
2024-12-10 14:10:28 +0300

    done practice 2

--- app.js
+++ app.js
@@ -8,12 +8,23 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-			b = prompt('На сколько оцените его?',''),
-			c = prompt('Один из последних просмотренных фильмов', ''),
-			d = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+for (let i = 0; i < 2; i++){
+	const a = prompt('Один из последних просмотренных фильмов', ''),
+				b = prompt('На сколько оцените его?','');
+	if (a != null && b != null && a != '' && b != '' && a.length < 50 ){
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+	} else{
+			console.log('error');
+			i--;
+	}
+}
+if (personalMovieDB.count < 10){
+	console.log('мало')
+}else if (personalMovieDB.count > 10 && personalMovieDB.count<30 ){
+	console.log('нормально')
+}else{
+	console.log('много')
+}
 console.log(personalMovieDB)
